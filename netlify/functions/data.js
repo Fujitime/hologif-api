c941ebcdffc0360a5551697398f84d2c114ba270
Fuji Halim Rabbani <racun1601@gmail.com>
2023-07-30 19:55:39 +0700

    change again

--- netlify/functions/data.js
+++ netlify/functions/data.js
@@ -1,10 +1,12 @@
-const data = require('../../img/info.json');
+const fs = require('fs');
+const path = require('path');
 
 exports.handler = async (event) => {
   // Add CORS headers
   const responseHeaders = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET',
+    'Vary': 'Origin',
   };
 
   if (event.httpMethod === 'OPTIONS') {
@@ -27,6 +29,10 @@ exports.handler = async (event) => {
         };
       }
 
+      const infoFilePath = path.join(__dirname, '../../img/info.json');
+      const rawData = fs.readFileSync(infoFilePath);
+      const data = JSON.parse(rawData);
+
       const member = data.hololive_members.find((m) => m.name.toLowerCase() === name.toLowerCase());
       if (!member) {
         return {
@@ -40,26 +46,30 @@ exports.handler = async (event) => {
         return {
           statusCode: 200,
           headers: responseHeaders,
-          body: JSON.stringify({ id: member.id, quote: member.quote }),
+          body: JSON.stringify({ quote: member.quote }),
         };
       } else if (format === 'gif') {
+        // Assume the `member.gif` contains the filename of the GIF in the img folder
+        const gifFilePath = path.join(__dirname, '../../img', member.gif);
+        const gifData = fs.readFileSync(gifFilePath, { encoding: 'base64' });
+
         return {
           statusCode: 200,
-          headers: responseHeaders,
-          body: JSON.stringify({ id: member.id, gif: member.gif }),
+          headers: { ...responseHeaders, 'Content-Type': 'application/json' },
+          body: JSON.stringify({ gif: `data:image/gif;base64,${gifData}` }),
         };
       } else if (format === 'social_media') {
         return {
           statusCode: 200,
           headers: responseHeaders,
-          body: JSON.stringify({ id: member.id, social_media: member.social_media }),
+          body: JSON.stringify({ social_media: member.social_media }),
         };
       } else {
         // Return the full member object if no specific format is requested
         return {
           statusCode: 200,
           headers: responseHeaders,
-          body: JSON.stringify({ id: member.id, ...member }),
+          body: JSON.stringify(member),
         };
       }
     }
